c0b2adfd6ec2fc6b2e6c6f2de02eec780eac3342
Ramy Elashri <ramyelashri@gmail.com>
2018-10-10 08:46:20 +0400

    Register and login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  NgModule,
+  Component,
+  Pipe,
+  OnInit
+} from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { APIService } from  '../api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +21,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm:FormGroup;
 
-  ngOnInit() {
+  constructor(private apiService: APIService) {
+  }
+
+  ngOnInit(){
+    this.loginForm = new FormGroup({
+      'emailInput': new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ]),
+      'passwordInput': new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  get emailInput() { return this.loginForm.get('emailInput'); }
+  get passwordInput() { return this.loginForm.get('passwordInput'); }
+
+  onSubmit(){
+    this.apiService.login(this.loginForm.value.emailInput, this.loginForm.value.passwordInput);
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  NgModule,
+  Component,
+  Pipe,
+  OnInit
+} from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { APIService } from  '../api.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +20,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm:FormGroup;
 
-  constructor() { }
+  constructor(private apiService: APIService) {
+  }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.registerForm = new FormGroup({
+      'emailInput': new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ]),
+      'passwordInput': new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  get emailInput() { return this.registerForm.get('emailInput'); }
+  get passwordInput() { return this.registerForm.get('passwordInput'); }
+
+  onSubmit(){
+    this.apiService.register(this.registerForm.value.emailInput, this.registerForm.value.passwordInput);
   }
 
 }
